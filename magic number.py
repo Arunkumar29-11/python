51b6de60683df697b1ecf7acc81963a8e0e09ae9
Arunkumar29-11 <marunkumar29112001@gmail.com>
2025-05-01 13:31:52 +0530

    update code

--- magic number.py
+++ magic number.py
@@ -4,7 +4,7 @@ attempts=0
 while attempts<5:
     print("guess a number between 1 and 20")
     guess=int(input())
-    attempts=attempts+1
+    attempts=attempts+2
     if guess==magic_number:
         break
 print("you have guess the magic number")
